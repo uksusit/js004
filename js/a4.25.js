0bea5fb0deb21e25f734fe9f9f9a3785ddd18a2e
uksusit <140089654+uksusit@users.noreply.github.com>
2024-09-29 21:38:58 +0300

    25tmp

---
+++ js/a4.25.js
@@ -0,0 +1,115 @@
+// Завдання 25/48
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику функції із завдання.
+
+const users = [
+    {
+        name: "Moore Hensley",
+        email: "moorehensley@indexia.com",
+        eyeColor: "blue",
+        friends: ["Sharron Pace"],
+        isActive: false,
+        balance: 2811,
+        gender: "male",
+        age: 37
+    },
+    {
+        name: "Sharlene Bush",
+        email: "sharlenebush@tubesys.com",
+        eyeColor: "blue",
+        friends: ["Briana Decker", "Sharron Pace"],
+        isActive: true,
+        balance: 3821,
+        gender: "female",
+        age: 34
+    },
+    {
+        name: "Ross Vazquez",
+        email: "rossvazquez@xinware.com",
+        eyeColor: "green",
+        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+        isActive: false,
+        balance: 3793,
+        gender: "male",
+        age: 24
+    },
+    {
+        name: "Elma Head",
+        email: "elmahead@omatom.com",
+        eyeColor: "green",
+        friends: ["Goldie Gentry", "Aisha Tran"],
+        isActive: true,
+        balance: 2278,
+        gender: "female",
+        age: 21
+    },
+    {
+        name: "Carey Barr",
+        email: "careybarr@nurali.com",
+        eyeColor: "blue",
+        friends: ["Jordan Sampson", "Eddie Strong"],
+        isActive: true,
+        balance: 3951,
+        gender: "male",
+        age: 27
+    },
+    {
+        name: "Blackburn Dotson",
+        email: "blackburndotson@furnigeer.com",
+        eyeColor: "brown",
+        friends: ["Jacklyn Lucas", "Linda Chapman"],
+        isActive: false,
+        balance: 1498,
+        gender: "male",
+        age: 38
+    },
+    {
+        name: "Sheree Anthony",
+        email: "shereeanthony@kog.com",
+        eyeColor: "brown",
+        friends: ["Goldie Gentry", "Briana Decker"],
+        isActive: true,
+        balance: 2764,
+        gender: "female",
+        age: 39
+    }
+];
+
+// До завдання
+// Завдання 25/48
+// Доповни функцію getFriends(users) таким чином, щоб вона повертала масив друзів всіх користувачів (властивість friends). У декількох користувачів можуть бути однакові друзі, зроби так, щоб масив, що повертається, не містив повторень.
+
+// Оголошена змінна getFriends
+// Змінній getFriends присвоєна стрілочна функція з параметром (users)
+// Виклик функції із зазначеним масивом користувачів повертає масив ["Sharron Pace", "Briana Decker", "Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner", "Goldie Gentry", "Aisha Tran", "Jordan Sampson", "Eddie Strong", "Jacklyn Lucas", "Linda Chapman"]
+// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+// // Change code below this line
+// const getFriends = (users) => {
+   
+// };
+// // Change code above this line
+
+// Change code below this line
+const allFr = [];
+    console.log(users);
+const getFriends = (users) => users.filter(user =>
+// user.friends) 
+{
+    allFr.push(...user.friends);
+    console.log(...user.friends);
+    // const uf = user.friends;
+    // console.log(...uf);
+    // allFr.push(...uf);
+    console.log(allFr);
+}
+  
+);
+console.log(allFr);
+
+    //  const afs = allFr.map(fr,idx,arr)
+    // return allFr;
+    
+// Change code above this line
+
+
+console.log(getFriends(users));
